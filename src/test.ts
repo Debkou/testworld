7975a0101aaea3505739a3e0eb3f7763e9706fa7
Dehbia Kouadria <kouadria.dehbia@fh-swf.de>
2024-02-08 13:21:56 +0100

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -2,5 +2,15 @@
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully');
 let currentPopup: any = undefined;
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
+
+    WA.room.area.onEnter('clock').subscribe(() => {
+        const today = new Date();
+        const time = today.getHours() + ":" + today.getMinutes();
+        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+    })
 Access-Control-Allow-Origin: *
 WA.chat.sendChatMessage('Hello world', 'Mr Robot');
