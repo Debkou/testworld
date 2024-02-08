244a189889fdd103e4bfad36a3c3cf103c360edd
Dehbia Kouadria <kouadria.dehbia@fh-swf.de>
2024-02-08 13:58:50 +0100

    update

--- tilesets/Doors.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Doors" tilewidth="32" tileheight="32" tilecount="192" columns="12">
- <image source="../Downloads/rpg_maker_vx___door_by_ayene_chan_d4grqwd.png" trans="402507" width="384" height="512"/>
-</tileset>
