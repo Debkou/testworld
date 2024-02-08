9ebd42dadda4ead4e3e46489820bc52b5f953d21
Dehbia Kouadria <kouadria.dehbia@fh-swf.de>
2024-02-08 13:20:17 +0100

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,5 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+console.log('Script started successfully');
 Access-Control-Allow-Origin: *
 WA.chat.sendChatMessage('Hello world', 'Mr Robot');
